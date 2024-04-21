56d6ec01684f0afdac4b53a9d01d380e1cb508dc
TemesgenUr <167687639+TemesgenUr@users.noreply.github.com>
2024-04-21 20:38:52 +0300

    Add files via upload

---
+++ String.c
@@ -0,0 +1,16 @@
+def character_frequency_count(string):
+    frequency = {}
+    for char in string:
+        if char in frequency:
+            frequency[char] += 1
+        else:
+            frequency[char] = 1
+    return frequency
+
+def main():
+    user_input = input("Enter a string: ")
+    count = character_frequency_count(user_input)
+    for char, frequency in count.items():
+        print(f"{char}: {frequency}")
+
+main()
